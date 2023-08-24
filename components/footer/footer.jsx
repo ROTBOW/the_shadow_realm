d0b5cd4e2144dfd05dc95d03fe343f0ca6919b66
Josiah Leon <sjosiahleon@gmail.com>
2023-08-23 21:49:14 -0700

    created footer

---
+++ components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './footer.scss';
+
+
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <div></div>
+            <div>Made with &nbsp;<s>ᗪEᒪIᖇIᑌᗰ</s><b>LOVE!</b>&nbsp; by <a href="https://github.com/ROTBOW" rel='noreferrer' target='_blank'>Josiah Leon</a></div>
+            <div>API created by <a href='https://db.ygoprodeck.com/'>YGOPRODeck</a></div>
+        </div>
+    )
+}
+
+export default Footer;
