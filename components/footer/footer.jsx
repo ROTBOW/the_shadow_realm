c03492311f69d0c3d9d87b6ae2bbc4be44b77757
Josiah Leon <sjosiahleon@gmail.com>
2023-09-06 10:38:36 -0700

    added buffer space for footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -6,11 +6,14 @@ import './footer.scss';
 const Footer = () => {
 
     return (
-        <div className="footer">
-            <div></div>
-            <div>Made with &nbsp;<s>ᗪEᒪIᖇIᑌᗰ</s><b>LOVE!</b>&nbsp; by <a href="https://github.com/ROTBOW" rel='noreferrer' target='_blank'>Josiah Leon</a></div>
-            <div>API created by <a href='https://db.ygoprodeck.com/'>YGOPRODeck</a></div>
-        </div>
+        <>
+            <div className="footer-spacer"/>
+            <div className="footer">
+                <div></div>
+                <div>Made with &nbsp;<s>ᗪEᒪIᖇIᑌᗰ</s><b>LOVE!</b>&nbsp; by <a href="https://github.com/ROTBOW" rel='noreferrer' target='_blank'>Josiah Leon</a></div>
+                <div>API created by <a href='https://db.ygoprodeck.com/'>YGOPRODeck</a></div>
+            </div>
+        </>
     )
 }
 
